f698693801121d713e2cf4a92518f0f9e21ffc16
Leoozito <leonardonogueira616@gmail.com>
2024-04-28 23:06:39 -0300

    feat: :memo: Adicionado nova parte do app
    Ensinado sobre lista

--- python-Alura/second-learning/01/first-app/main.py
+++ python-Alura/second-learning/01/first-app/main.py
@@ -1,6 +1,8 @@
 import os
 import time 
 
+all_restaurants = []
+
 def apresentation_of_program():
     print("""
     SEJA BEM VINDO AO -
@@ -23,27 +25,46 @@ def apresentation_of_options():
 """)
     
 def choose_option():
-    chosen_option = int(input("Digite o n° da opção desejada: "))
-    
-    if chosen_option == 1:
-        register_restaurant()
-    elif chosen_option == 2:
-        list_restaurant()
-    elif chosen_option == 3:
-        activate_restaurant()
-    elif chosen_option == 4:
-        exit_program()
-    else:
-        os.system("clear") 
-        print("Opção invalida !  ✘")
+    try:
+        chosen_option = int(input("Digite o n° da opção desejada: "))
+        
+        if chosen_option == 1:
+            register_restaurant()
+        elif chosen_option == 2:
+            list_restaurant()
+        elif chosen_option == 3:
+            activate_restaurant()
+        elif chosen_option == 4:
+            exit_program()
+        else:
+            os.system("clear") 
+            print("Opção invalida !  ✘")
+            time.sleep(3)
+            apresentation_of_options()
+    except:
+        os.system("clear")
+        print("Digite apenas números !")
         time.sleep(3)
-        apresentation_of_options()
+        os.system('clear')
+        main()
 
 def register_restaurant():
-    print("Cadastrar restaurante")
+    os.system('clear')
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print("Cadastro de restaurante\n")
+    name_restaurant = input("Digite o nome do restaurante: ")
+    all_restaurants.append(name_restaurant)
+    os.system('clear')
+    print(f"Restaurante {name_restaurant} cadastrado com sucesso !!\n\n")
+    input("Clique qualquer tecla para voltar ao menu principal !")
+    main()
 
 def list_restaurant():
-    print("Restaurantes")
+    os.system('clear')
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print("Restaurantes cadastrados\n")
+    for restaurants in all_restaurants:
+        print(restaurants)
 
 def activate_restaurant(): 
     print("Ativar restaurante")
