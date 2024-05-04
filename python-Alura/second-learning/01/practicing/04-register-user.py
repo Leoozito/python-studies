0a365290068eca081aedf15a9c961bdb39f35bff
Leoozito <leonardonogueira616@gmail.com>
2024-05-03 23:41:16 -0300

    feat: :white_check_mark: Atualizando projeto de pratica
    Sistema de usuario
    
    utilizando as praticas de lista

--- python-Alura/second-learning/01/practicing/04-register-user.py
+++ python-Alura/second-learning/01/practicing/04-register-user.py
@@ -1,6 +1,44 @@
 import os
+import time
 
 users_registered = [{'nome':'Leonardo', 'idade':'21 anos', 'cidade':'São Paulo'}]
+
+def menu_options():
+    apresentation_tile("Sistema de Usuario")
+    
+    try:
+        choice = int(input("""
+            Tecle:
+            [ 1 ] Cadastro de novo usuario 
+            [ 2 ] Editar novo usuario
+            [ 3 ] Listar usuarios
+
+        """))
+        if choice == 1:
+            post_user()
+        elif choice == 2:
+            update_user()
+        elif choice == 3:
+            list_user()
+        else:
+            print("Opção invalida, tecle um número referente as opções")
+            time.sleep(3)
+            menu_options()
+    expect NameError:
+        os.system("clear")
+        print("Digite apenas números !")
+        time.sleep(3)
+        os.system('clear')
+        menu_options()
+
+def post_user():
+    pass
+
+def update_user():
+    pass
+    
+def list_user():
+    pass
 
 def update_register_user():
     user_insert = str(input("Digite o nome do usuário: "))
@@ -14,8 +52,8 @@ def update_register_user():
         print("Usuario não encontrado")
 
 def menu_update(user):
-    apresentation("Dados do Usuário")
-    print(f'{"NOME".ljust(20)} {"IDADE".ljust(20)} {"CIDADE"}')
+    apresentation_title("Dados do Usuário")
+    print(f'{"NOME".ljust(22)} {"IDADE".ljust(22)} {"CIDADE"}')
     print(f"{user['nome'].ljust(20)} | {user['idade'].ljust(20)} | {user['cidade']}")
     choice = int(input("""
         O que gostaria de editar?
@@ -36,23 +74,23 @@ def menu_update(user):
         print("Opção invalida\n")
         menu_update()
 
-def apresentation(text):
+def apresentation_title(text):
     os.system('clear')
     print("=-" * len(text))
     print(f"""          {text.upper()}\n""")
 
 def update_name(user):
-    apresentation("Editar NOME do usuário")
+    apresentation_title("Editar NOME do usuário")
     new_name = str(input("Digite novo nome desejado: "))
     confirm_update_datas('nome', new_name, user['nome'])
 
 def update_years(user):
-    apresentation("Editar IDADE do usuário")
-    new_year = str(input("Digite nova idade: "))
+    apresentation_title("Editar IDADE do usuário")
+    new_year = int(input("Digite nova idade: "))
     confirm_update_datas('idade', new_year, user['idade'])
 
 def update_city(user):
-    apresentation("Editar CIDADE do usuário")
+    apresentation_title("Editar CIDADE do usuário")
     new_city = str(input("Digite a nova cidade: "))
     confirm_update_datas('idade', new_city, user['cidade'])
 
